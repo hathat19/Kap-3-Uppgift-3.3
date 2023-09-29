aab8a3c6a24583b7f8fd95bb5f57d3d89ec11146
humy06001 <humy06001@E-PW0234KS>
2023-09-29 10:41:19 +0200

    Add project files.

---
+++ Kap 3 Uppgift 3.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Uppgift3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra bilen? Svara med ett heltal.");
+            int hours = int.Parse(Console.ReadLine());
+
+            //Är sum över 950? I så fall så stannar den på 950.
+            int sum = (hours*80) > 950 ? 950 : (hours*80);
+
+            //Timme eller timmar
+            string timme = hours == 1 ? "timme" : "timmar";
+
+            Console.WriteLine($"Det kostar {sum} kronor att hyra bilen i {hours} {timme}.");
+            Console.ReadKey();
+        }
+    }
+}
